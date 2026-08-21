4e91b1a37b83c9ef5fb31e229c7716b492725472
aeronoponda-glitch <aeronoponda@gmail.com>
2026-08-21 13:40:26 +0800

    ..

---
+++ main (3).c
@@ -0,0 +1,22 @@
+/*OPONDA, AERON D.*/
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+
+    
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+
+    
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+
+   
+    result = num1 * num2;
+
+   
+    printf("The result of %.2f multiplied by %.2f is: %.2f\n", num1, num2, result);
+
+    return 0;
+}
