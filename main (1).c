937d4063479766cf0fd463ebdbc9b00df8c3f90a
aeronoponda-glitch <aeronoponda@gmail.com>
2026-08-21 13:29:39 +0800

    ,,

---
+++ main (1).c
@@ -0,0 +1,25 @@
+/*OPONDA, AERON D.*/
+#include <stdio.h>
+
+int main() {
+    
+    // METHOD 1: Large ASCII Heart
+    
+    printf("   ***     ***   \n");
+    printf("  *****   *****  \n");
+    printf(" ******* ******* \n");
+    printf(" *************** \n");
+    printf("  *************  \n");
+    printf("   ***********   \n");
+    printf("    *********    \n");
+    printf("     *******     \n");
+    printf("      *****      \n");
+    printf("       ***       \n");
+    printf("        *        \n\n");
+    printf("  +-----------+ \n");
+    printf("  |  [+1] <3  | \n");
+    printf("  |   LIKE    | \n");
+    printf("  +-----------+ \n\n");
+
+    return 0;
+}
